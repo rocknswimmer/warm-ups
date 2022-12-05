fb5fa9fab1f440b8638951a2020012b49287d91a
Andy Simmons <rocknswimmer116@gmail.com>
2022-12-05 10:45:37 -0500

    was overcomplicating the solution. trimmed off the extra stuff and its working bad %s though

--- valid palindrome.js
+++ valid palindrome.js
@@ -3,58 +3,31 @@
  * @return {boolean}
  */
  var isPalindrome = function(s) {
-  let i = 0;
-  let j = s.length - 1;
+    let i = 0;
+    let j = s.length - 1;
 
 
-  while(i < s.length && j >= 0) {
-      if (i === j) {
+    while(i < j ) {
 
-              return true;
-          }
-      if (i + 1 === j && (s[i].toUpperCase() === s[j].toUpperCase())) {
-          return true;
-      }
+            while(i < j && (alp.indexOf(s[i].toUpperCase()) === -1 && num.indexOf(s[i]) === -1)) {
+                i++;
+            }
+           while(i < j && (alp.indexOf(s[j].toUpperCase()) === -1 && num.indexOf(s[j]) === -1)) {
+                j--;
+            }
+            if(s[i].toUpperCase() !== s[j].toUpperCase()) {
 
-
-      if (i > j ) {
-
-                  return false;
-              }
-
-          while(alp.indexOf(s[i].toUpperCase()) === -1) {
-              i++;
-              if (i > j ) {
-
-                  let copy = s.slice();
-  let arr = copy.split('');
-  let noLets = arr.map((letter) => {
-      return alp.indexOf(letter);
-  }).every((ind) => {
-      return ind === -1;
-  })
-  return noLets;
-              }
-          }
-         while(alp.indexOf(s[j].toUpperCase()) === -1) {
-              j--;
-              if (i > j ) {
-
-                  return false;
-              }
-          }
-          if(s[i].toUpperCase() !== s[j].toUpperCase()) {
-
-              return false;
-          }
+                return false;
+            }
 
 
-      i++;
-      j--;
-      }
+        i++;
+        j--;
+        }
 
 
-  return true;
+    return true;
 };
 
 var alp = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var num = ['1','2','3','4','5','6','7','8','9','0'];
