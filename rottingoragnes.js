35ecf33462998668dbfd646f6d725568f3f1ee1f
Andy Simmons <rocknswimmer116@gmail.com>
2023-02-09 20:45:27 -0500

    got bored durring Q&A 25% speed 6% mem

--- rottingoragnes.js
+++ rottingoragnes.js
@@ -1,13 +1,13 @@
 var orangesRotting = function(grid) {
   var spread = true;
   var times = 0;
-  var before = grid.map((arr) => {return arr})
-  var after = grid.map((arr) => {return arr})
+  var before = grid.map((arr) => {return arr.slice()})
+  var after = grid.map((arr) => {return arr.slice()})
 
   while(spread){
       spread = false;
       times++
-      after = before.map((arr) => {return arr})
+      after = before.map((arr) => {return arr.slice()})
       for(let i = 0; i < before.length; i++){
           for(let j = 0; j < before[0].length; j++){
               if(before[i][j] === 2){
@@ -29,12 +29,16 @@ var orangesRotting = function(grid) {
               }
               }
           }
+
       }
-      console.log( after)
-      before = after.map((arr) => {return arr})
+      if(spread === false){
+              times--
+          }
+
+      before = after.map((arr) => {return arr.slice()})
   }
-  console.log(times, after)
-  if(times > 0/*perform and every finding index of 1 for all arrays = -1*/ ){
+
+  if(after.every((row) => {return row.indexOf(1) === -1})/*perform and every finding index of 1 for all arrays = -1*/ ){
       return times
   }
   return -1;
