570812ad2f488503958a7535730c037974995a11
Andy Simmons <rocknswimmer116@gmail.com>
2023-02-09 20:00:22 -0500

    save first thought before abab meeting

---
+++ rottingoragnes.js
@@ -0,0 +1,41 @@
+var orangesRotting = function(grid) {
+  var spread = true;
+  var times = 0;
+  var before = grid.map((arr) => {return arr})
+  var after = grid.map((arr) => {return arr})
+
+  while(spread){
+      spread = false;
+      times++
+      after = before.map((arr) => {return arr})
+      for(let i = 0; i < before.length; i++){
+          for(let j = 0; j < before[0].length; j++){
+              if(before[i][j] === 2){
+                  if(after[i + 1] !== undefined && after[i + 1][j] === 1){
+                  spread = true
+                  after[i + 1][j] = 2
+              }
+              if(after[i - 1] !== undefined && after[i - 1][j] === 1){
+                  spread = true
+                  after[i - 1][j] = 2
+              }
+              if(after[i][j + 1] === 1){
+                  spread = true
+                  after[i][j + 1] = 2
+              }
+              if(after[i][j - 1] === 1){
+                  spread = true
+                  after[i][j - 1] = 2
+              }
+              }
+          }
+      }
+      console.log( after)
+      before = after.map((arr) => {return arr})
+  }
+  console.log(times, after)
+  if(times > 0/*perform and every finding index of 1 for all arrays = -1*/ ){
+      return times
+  }
+  return -1;
+};
