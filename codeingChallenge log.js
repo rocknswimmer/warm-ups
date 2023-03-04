715e71446e5babf7e081e957c248e537d21f110f
Andy Simmons <rocknswimmer116@gmail.com>
2023-03-04 13:29:53 -0500

    challenge update

--- codeingChallenge log.js
+++ codeingChallenge log.js
@@ -3,3 +3,4 @@
 
 //2/1/23 vanguard basic test
 //2/8/23 confident test simple algorithms, then abused with route learning ?'s about html and css
+//3/4 moody though coding 2 bitwise overcomplication of hard-med problems and a sql with a fun group by with average
