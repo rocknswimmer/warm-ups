57e3cb947eccdf029ffa1ef783899b5ec110468e
Andy Simmons <rocknswimmer116@gmail.com>
2023-03-16 16:43:58 -0400

    completed a python code challenge!

--- codeingChallenge log.js
+++ codeingChallenge log.js
@@ -5,3 +5,4 @@
 //2/8/23 confident test simple algorithms, then abused with route learning ?'s about html and css
 //3/4 moody though coding 2 bitwise overcomplication of hard-med problems and a sql with a fun group by with average
 // 3/5 cisco had to take in python and the accidentally submitted test after only answering the last ?
+// 3/16 koddi another python test, this time I did not early submit and managed to answer both despite a tab + space default spacing that threw errors on the first questions complicating my learning durring the challenge
