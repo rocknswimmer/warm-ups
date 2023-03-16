93f36075f96361c4761fc4f9dc3f040925591706
Andy Simmons <rocknswimmer116@gmail.com>
2023-03-16 16:45:08 -0400

    adjusted begining history

--- codeingChallenge log.js
+++ codeingChallenge log.js
@@ -1,6 +1,6 @@
 // I feel an hour long coding challenge taken for a job or in interview should count in git hub tracking so heres the log
 
-
+//1-3 before I started this log
 //2/1/23 vanguard basic test
 //2/8/23 confident test simple algorithms, then abused with route learning ?'s about html and css
 //3/4 moody though coding 2 bitwise overcomplication of hard-med problems and a sql with a fun group by with average
