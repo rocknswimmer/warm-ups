8d2c93a0c5ded64760c6d584a5af7c8ba1af5777
Andy Simmons <rocknswimmer116@gmail.com>
2023-03-22 17:16:59 -0400

    though but potentially successful challenge

--- codeingChallenge log.js
+++ codeingChallenge log.js
@@ -6,3 +6,4 @@
 //3/4 moody though coding 2 bitwise overcomplication of hard-med problems and a sql with a fun group by with average
 // 3/5 cisco had to take in python and the accidentally submitted test after only answering the last ?
 // 3/16 koddi another python test, this time I did not early submit and managed to answer both despite a tab + space default spacing that threw errors on the first questions complicating my learning durring the challenge
+//3/22/23 smoothstack, pretty challengeing ? if I read it right, requireing the inputs to the function to be checked as meeting constraints, adding to the challenge over a leetcode version of the problem got an 86% even without a solution to the question that wanted a function written in bash
