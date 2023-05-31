61d917177f63602f16dcdb7743175a2bf81cc819
Andy Simmons <rocknswimmer116@gmail.com>
2023-05-31 11:33:39 -0400

    schema outline

--- challenge.sql
+++ challenge.sql
@@ -1,1 +1,12 @@
 -- space for sql schema and filler, may help with date/time questions on relay app
+
+drop table if exists Donations;
+drop table if exists People;
+
+create table People (
+
+);
+
+create table Donations (
+
+);
