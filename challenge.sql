0281a0a7210fb2479280a0c048d5661e3ac8b33d
Andy Simmons <rocknswimmer116@gmail.com>
2023-06-05 12:45:44 -0400

    ideas before researching

--- challenge.sql
+++ challenge.sql
@@ -45,3 +45,7 @@ insert into Donations (People_ID, Donation_Date, Party) values (4, 7, 1)
 -- need to return name, most recent donation, political party guess, Address
 -- extra-still works with no donations in
 
+-- individual statements before combining
+-- select fullName, streetAddress from People
+-- figure out how to pull just largest value, like converting date to unix if no better way to do dates
+-- sum up the values in party where fullName matches, should use ID's but not sure how to handle multiple similar full names in SQL based off promt and have already created my own db and seed file to attempt the promt, if positive R if negative D if even 0, questioned political statements of positive and negative
