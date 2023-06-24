03ae4bc53f5a01db0af9715714f6962d2a700aa5
Andy Simmons <rocknswimmer116@gmail.com>
2023-06-23 21:47:51 -0400

    travel

--- codeingChallenge log.js
+++ codeingChallenge log.js
@@ -22,6 +22,7 @@
 //logic or psych assessments
 //5/18 answered 38/40 ?s in 20 mnutes
 //5/20 getting more and more kinda similar logic ones got like 40/50 in  15 but two were speed guess/skips
+//6/23 int commute
 
 //learning xp
 // GitLabs DevSecOps 6/15
